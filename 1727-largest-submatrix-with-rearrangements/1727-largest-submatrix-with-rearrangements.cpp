04d655b987d00ece54d58f2bb4dbee13e44395b3
Aady.5 <147737356+Addyamishra@users.noreply.github.com>
2026-03-18 00:19:49 +0530

    Time: 199 ms (5.32%), Space: 145.2 MB (5.85%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,46 +1,26 @@
 class Solution {
-public:  
-    static int largestSubmatrix(vector<vector<int>>& matrix) {
-        const int m=matrix.size(), n=matrix[0].size();
-        int area=count(matrix[0].begin(), matrix[0].end(), 1);
-        if (m==1) return area;
-        if (n==1){
-            for(int i=1; i<m; i++){
-                matrix[i][0]+=matrix[i][0]*matrix[i-1][0];
-                area=max(area, matrix[i][0]);
+public:
+    int largestSubmatrix(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=1;j<n;j++){
+               if(mat[j][i]==0) continue;
+               mat[j][i]+=mat[j-1][i];
             }
-            return area;
         }
-        
-        for(int i=1; i<m; i++){
-            for(int j=0;  j<n; j++){
-                matrix[i][j]+=matrix[i][j]*matrix[i-1][j];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            multiset<int> cont;
+            int c=0;
+            for(int j=0;j<m;j++){
+            if(mat[i][j]>0) cont.insert(mat[i][j]),c++;
             }
-            const auto& row=matrix[i];
-            int minH=i+1, maxH=0;
-            for(int x: row){
-                minH=min(minH, x);
-                maxH=max(maxH, x);
+            int ci=0;
+            for(int i:cont){
+                ans=max(ans,(c-ci)*i);
+                ci++;
             }
-            vector<int> freq(maxH-minH+1, 0);
-            for(int x: row){
-                freq[x-minH]++;
-            }
-            int acc=0;
-            for(int x=maxH-minH; acc<n; x--){
-                if (freq[x]>0){
-                    acc+=freq[x];
-                    area=max(area, acc*(x+minH));
-                }
-            }  
         }
-        return area;
+     return ans;
     }
 };
-// auto init = []()
-// { 
-//     ios::sync_with_stdio(0);
-//     cin.tie(0);
-//     cout.tie(0);
-//     return 'c';
-// }();
