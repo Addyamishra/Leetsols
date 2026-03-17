69392cda745f152e50f0c6a1f491af416e073d7c
Aady.5 <147737356+Addyamishra@users.noreply.github.com>
2026-03-17 12:40:21 +0530

    Time: 8 ms (97.87%), Space: 81.2 MB (77.66%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -37,10 +37,10 @@ public:
         return area;
     }
 };
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
+// auto init = []()
+// { 
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 'c';
+// }();
