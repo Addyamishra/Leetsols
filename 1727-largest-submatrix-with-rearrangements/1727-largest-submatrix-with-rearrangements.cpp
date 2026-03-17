d25ea15d531e5def7494bd226930598a8d7706bb
Aady.5 <147737356+Addyamishra@users.noreply.github.com>
2026-03-18 00:35:33 +0530

    Time: 20 ms (82.45%), Space: 89.2 MB (61.7%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int largestSubmatrix(vector<vector<int>>& mat) {
-        int n=mat.size(),m=mat[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=1;j<n;j++){
-               if(mat[j][i]==0) continue;
-               mat[j][i]+=mat[j-1][i];
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int ans = 0;
+        
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (matrix[row][col] != 0 && row > 0) {
+                    matrix[row][col] += matrix[row - 1][col];
+                }
+            }
+            
+            vector<int> currRow = matrix[row];
+            sort(currRow.begin(), currRow.end(), greater());
+            for (int i = 0; i < n; i++) {
+                ans = max(ans, currRow[i] * (i + 1));
             }
         }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            multiset<int> cont;
-            int c=0;
-            for(int j=0;j<m;j++){
-            if(mat[i][j]>0) cont.insert(mat[i][j]),c++;
-            }
-            int ci=0;
-            for(int i:cont){
-                ans=max(ans,(c-ci)*i);
-                ci++;
-            }
-        }
-     return ans;
+        
+        return ans;
     }
 };
