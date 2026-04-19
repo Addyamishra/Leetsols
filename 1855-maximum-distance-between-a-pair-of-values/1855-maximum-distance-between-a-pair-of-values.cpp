24f0269df6875a4cb4f91a9096d02147e1822fbc
Aady.5 <147737356+Addyamishra@users.noreply.github.com>
2026-04-20 01:03:27 +0530

    Time: 0 ms (100%), Space: 102.1 MB (97.12%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -4,11 +4,10 @@ public:
         int i = 0, j = 0, ans = 0;
 
         while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] <= nums2[j]) {
+            while(i<nums1.size() && nums1[i]>nums2[j]) i++;
+            if (i<nums1.size() && nums1[i] <= nums2[j]) {
                 ans = max(ans, j - i);
                 j++;
-            } else {
-                i++;
             }
         }
 
