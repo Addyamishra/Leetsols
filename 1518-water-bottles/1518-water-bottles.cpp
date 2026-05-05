dbc9a9d92d044ea344ba8c284a44d10739e5f962
Aady.5 <147737356+Addyamishra@users.noreply.github.com>
2026-05-05 13:55:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (43.91%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int numWaterBottles(int nb, int ne) {
       int ans=0;
-       while(nb>=ne){
-       int d=nb/ne;
-       ans+=(d*ne);
-       nb-=(d*ne);
-       nb+=d;
-       }
-       return ans+nb;
+    //    while(nb>=ne){
+    //    int d=nb/ne;
+    //    ans+=(d*ne);
+    //    nb-=(d*ne);
+    //    nb+=d;
+    //    }
+       return nb+(nb-1)/(ne-1);
     }
 };
